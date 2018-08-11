fcfcd44c86fbf40b6c5e83651c7481d74aa67017
Matheus Nascimento <contamatheusnascimento@gmail.com>
2018-08-10 22:51:33 -0300

    Amend commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,19 @@
 #include <test.h>
 #include <opencv2/highgui.hpp>
 
+struct car
+{
+    int model;
+    int color;
+    void getFipTableValue()
+    {
+        std::cout << "FIP VALUE\n";
+    }
+}gol;
+
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello Moto!" << std::endl;
+    gol.getFipTableValue();
     return 0;
 }
