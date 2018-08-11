c15fd25802ff3b4610a2f6e3a605feabdba5f006
Matheus Nascimento <contamatheusnascimento@gmail.com>
2018-08-11 00:56:07 -0300

    git develop color line

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,40 @@
 struct car
 {
     int model;
-    int color;
+    int color_develop;
+    car()
+    : model(2)
+    , color(1)
+    {
+        std::cout << "Struct constructor" << std::endl;
+    }
     void getFipTableValue()
     {
         std::cout << "FIP VALUE\n";
     }
-}gol;
+};
+
+int& sum(int& x, int& y, const char* string)
+{
+    std::cout << string << std::endl;
+    static int c = x+y;
+    x = c;
+    y = c -1;
+    return c;
+}
 
 int main()
 {
     std::cout << "Hello Moto!" << std::endl;
+    const char *string = "Isso eh uma string ou um ponteiro?";
+
+    car gol;
+
+    int x = 2, y = 3;
+    auto val = sum(x,y,string);
+    sum(x,y,string);
+    sum(x,y,string);
     gol.getFipTableValue();
+    std::cout << gol.color << std::endl;
     return 0;
 }
